07ef2f335078ce8b8e202e542bd1763a6a40605c
D9m0n <c2h8e9d4@gmail.com>
2020-06-06 21:27:25 +0300

    Finish base framework fnl

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,17 +2,18 @@ import {$} from '../../core/DOM';
 
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector);
+    this.$el = $(selector);
     this.components = options.components || [];
   }
 
   getRoot() {
     const $root = $.create('div', 'excel')
-    this.components.forEach(Component => {
+    this.components = this.components.map(Component => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
+      return component
     });
     return $root
   }
@@ -20,5 +21,6 @@ export class Excel {
   render() {
     // this.$el.insertAdjacentHTML('afterbegin', ``)
     this.$el.append(this.getRoot())
+    this.components.forEach(component => component.init());
   }
 }
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,8 +1,35 @@
+import {capitalize} from './utils'
+
 export class DomListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error(`No $root provided for DomListener`)
     }
-    this.$root
+    this.$root = $root
+    this.listeners = listeners
   }
+
+  initDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      if (!this[method]) {
+        const name = this.name || ''
+        throw new Error(
+            `Method ${method} is not implemented if ${name} component` )
+      }
+      this[method] = this[method].bind(this)
+      this.$root.on(listener, this[method])
+    })
+  }
+
+  removeDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      this.$root.off(listener, this[method])
+    })
+  }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
 }
